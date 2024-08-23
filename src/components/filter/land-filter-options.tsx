97e149738c796794af45f773e02b058d890ff5af
Kwame007 <clementnyankuadjei@gmail.com>
2024-08-23 02:41:06 +0000

    wip : add range

--- src/components/filter/land-filter-options.tsx
+++ src/components/filter/land-filter-options.tsx
@@ -1,8 +1,10 @@
-import { Separator } from "../ui/separator";
-import { PriceInput } from "./custom-price-input";
+import { siteConfig } from "@/config/site"
+
+import { Separator } from "../ui/separator"
+import { PriceInput } from "./custom-price-input"
 
 // options for land
-export function LandFilterOptions(){
+export function LandFilterOptions() {
   return (
     <div className="flex items-end justify-center gap-2 py-1">
       <PriceInput
@@ -10,6 +12,7 @@ export function LandFilterOptions(){
         unit="GH₵"
         placeholder={{ min: "Min.price", max: "Max.price" }}
         className="h-[20px] border-none px-5 py-0 text-white"
+        range={siteConfig.priceRange}
       />
       <Separator orientation="vertical" className="h-[20px] bg-[#9A9EB5]" />
       <PriceInput
@@ -17,6 +20,7 @@ export function LandFilterOptions(){
         unit="m²"
         placeholder={{ min: "Min.area", max: "Max.area" }}
         className="h-[20px] border-none px-5 py-0 text-white"
+        range={siteConfig.priceRange}
       />
     </div>
   )
